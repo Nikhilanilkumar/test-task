ad316024c720944cd5e427e1cf24e7df149b0a96
Nikhilanilkumar <44563506+Nikhilanilkumar@users.noreply.github.com>
2018-10-29 11:20:04 +0530

    Add files via upload

---
+++ Task-PHP/classes/BankAccount.php
@@ -0,0 +1,37 @@
+<?php
+
+class BankAccount implements IfaceBankAccount
+{
+
+    private $balance = null;
+
+    public function __construct(Money $openingBalance)
+    {
+        $this->balance = $openingBalance;
+    }
+
+    public function balance()
+    {
+        return $this->balance;
+    }
+
+    public function deposit(Money $amount)
+    {
+       $this->balance=new Money($this->balance()->value()+$amount->value());
+    }
+
+    public function transfer(Money $amount, BankAccount $account)
+    {
+           $this->withdraw($amount);
+        $account->balance=new Money($account->balance()->value()+$amount->value());
+    }
+	
+	public function withdraw(Money $amount)
+    {
+          if ($this->balance()->value() > $amount->value() ) {
+            $this->balance=new Money($this->balance()->value()-$amount->value());
+        }else{
+            throw new Exception("Withdrawl amount larger than balance");
+        }
+    }
+}
